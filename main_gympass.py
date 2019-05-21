3a9a2da9f5ebabbfb91e7c89751f95b0b73a8109
Gasque, Guilherme <shazan_0@hotmail.com>
2019-05-21 14:21:24 -0300

    Update main_gympass.py

--- main_gympass.py
+++ main_gympass.py
@@ -14,8 +14,8 @@ import numpy as np
 import subprocess
 
 usuario = 'guilherme.gasque'
-#os.chdir("C:/Users/" + usuario + "/OneDrive - EDITORA E DISTRIBUIDORA EDUCACIONAL S A/Python/robot/codigos")
-os.chdir("C:/Users/guilherme.gasque/OneDrive - EDITORA E DISTRIBUIDORA EDUCACIONAL S A/Python/robot/codigos")
+
+os.chdir("C:/Users/guilherme.gasque/Python/robot/codigos")
 from funcoes_download_automatico_gympass import *
 
 #arquivo_json_de_parametros = carregaParametrosJson('parametros_login.json')
